c4e12b75a2fe32717c42dc2013fa775a9ff0306a
Yujin <112697788+rkdwlsdb@users.noreply.github.com>
2024-11-04 20:21:44 +0900

    [Bronze I] Title: ROT13, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11655. ROT13/ROT13.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string s;
+
+int main(){
+    getline(cin, s);
+    for(int i=0; i<s.size(); i++){
+        if(s[i]>=65 && s[i]<=90){
+            if(s[i]+13 > 90) s[i] = s[i] + 13 - 26;
+            else s[i] += 13;
+        }
+        else if(s[i]>=97 && s[i]<=122){
+            if(s[i]+13 > 122) s[i] = s[i] + 13 - 26;
+            else s[i] += 13;
+        }
+    }
+    for(int i=0; i<s.size(); i++) cout << s[i];
+    cout << '\n';
+    return 0;
+}
