128fdb31824f69e028e788f6c6d976c6b29db47b
Yujin <112697788+rkdwlsdb@users.noreply.github.com>
2025-01-05 16:54:33 +0900

    [Silver V] Title: 기상캐스터, Time: 0 ms, Memory: 2068 KB -BaekjoonHub

--- 백준/Silver/10709. 기상캐스터/기상캐스터.cc
+++ 백준/Silver/10709. 기상캐스터/기상캐스터.cc
@@ -1,36 +1,34 @@
 #include<bits/stdc++.h>
 using namespace std;
-/*- c를 만날때까지 -1
-- c를 만나면 0으로 바꾸고 다음 c를 만날때까지 ++
-- 다음 c를 만나면 또 0으로 바꾸고*/
 
-int h, w, a[104][104], c[104][104];
-char s[104];
+int h, w, a[104][104];
+string s;
 
 int main(){
     cin >> h >> w;
     for(int i=0; i<h; i++){
-        scanf("%s", &s);
+        cin >> s;
         for(int j=0; j<w; j++){
-            a[i][j] = s[j];
+            if(s[j]=='.') a[i][j] = -1;
+            else a[i][j] = 0;
         }
     }
     for(int i=0; i<h; i++){
-        int min=0;
         for(int j=0; j<w; j++){
-            if(a[i][j] == 99){ // c일때 
-                c[i][j] = 0;
-                min = 1;
+            if(a[i][j] == 0){
+                int cnt = 1;
+                while(a[i][j+1] != 0){
+                    a[i][j+1] = cnt++;
+                    j++;
+                }
             }
-            if(min == 0 && a[i][j] != 99) c[i][j] = -1;
-            if(min >= 1 && a[i][j] != 99) c[i][j] = min++;            
         }
     }
     for(int i=0; i<h; i++){
         for(int j=0; j<w; j++){
-            printf("%d ", c[i][j]);
+            cout << a[i][j] << ' ';
         }
-        printf("\n");
+        cout << '\n';
     }
     return 0;
 }
